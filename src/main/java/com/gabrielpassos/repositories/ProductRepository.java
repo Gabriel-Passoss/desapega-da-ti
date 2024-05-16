14399d24d8b9ffc78532abb9c9ea84a32eed9411
Rodrigo Luigi <digaor@gmail.com>
2024-05-16 14:28:59 -0300

    refact: ProductReposity

--- src/main/java/com/gabrielpassos/repositories/ProductRepository.java
+++ src/main/java/com/gabrielpassos/repositories/ProductRepository.java
@@ -1,15 +1,19 @@
 package com.gabrielpassos.repositories;
 
 import com.gabrielpassos.entities.Product;
+import java.util.ArrayList;
 
 import java.util.Optional;
 
 public interface ProductRepository {
+
     public Optional<Product> findProductByName(String name);
 
-    public void create(Product product);
+    public ArrayList findAll();
 
-    public void update(Product product);
+    public boolean create(Product product);
 
-    public void delete(Product product);
+    public boolean update(Product product);
+
+    public boolean delete(Product product);
 }
