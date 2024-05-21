2346d62775e7e77e25fc7c2d121afd97539348b0
Gabriel Passos <contato.gabrielpassos@gmail.com>
2024-05-21 17:48:07 -0300

    add index ui

--- src/main/java/com/gabrielpassos/Main.java
+++ src/main/java/com/gabrielpassos/Main.java
@@ -1,7 +1,11 @@
 package com.gabrielpassos;
 
+import com.gabrielpassos.ui.Index;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var panel = new Index();
+                
+        panel.setVisible(true);
     }
 }
