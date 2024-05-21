ea1be1f1a39f9f5fc3bbfa4b32622b705cf2d0d1
Rodrigo Luigi <digaor@gmail.com>
2024-05-20 23:29:53 -0300

    feat: create product service, getMaxid method and implement create method in ProductDAO

--- src/main/java/com/gabrielpassos/dao/ProductDAO.java
+++ src/main/java/com/gabrielpassos/dao/ProductDAO.java
@@ -3,6 +3,9 @@ package com.gabrielpassos.dao;
 import com.gabrielpassos.database.MySqlDriver;
 import com.gabrielpassos.entities.Product;
 import com.gabrielpassos.repositories.ProductRepository;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,24 @@ public class ProductDAO implements ProductRepository {
     public ProductDAO() {
         this.db = new MySqlDriver();
         this.productList = new ArrayList<>();
+    }
+
+    public int getMaxID() {
+        String sql = "SELECT MAX(id) id FROM products";
+
+        try (PreparedStatement stmt = db.getConnection().prepareStatement(sql)) {
+            ResultSet res = stmt.executeQuery();
+
+            if (res.next()) {
+
+                return res.getInt("id");
+            } else {
+                return 0;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter o maior ID: " + e.getMessage());
+        }
     }
 
     @Override
@@ -30,7 +51,24 @@ public class ProductDAO implements ProductRepository {
 
     @Override
     public boolean create(Product product) {
-        return true;
+        String sql = "INSERT INTO products(id, name, description, quantity, price, created_at) VALUES(?,?,?,?,?,?)";
+
+        try (PreparedStatement stmt = db.getConnection().prepareStatement(sql)) {
+
+            stmt.setInt(1, product.getId());
+            stmt.setString(2, product.getName());
+            stmt.setString(3, product.getDescription());
+            stmt.setInt(4, product.getQuantity());
+            stmt.setDouble(5, product.getPrice());
+            stmt.setDate(6, new java.sql.Date(product.getCreatedAt().getTime()));
+
+            stmt.execute();
+
+            return true;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao criar o produto: " + e.getMessage());
+        }
     }
 
     @Override
---
+++ src/main/java/com/gabrielpassos/services/ProductService.java
@@ -0,0 +1,68 @@
+package com.gabrielpassos.services;
+
+import com.gabrielpassos.entities.Product;
+import com.gabrielpassos.dao.ProductDAO;
+
+import java.util.Date;
+import java.util.List;
+
+public class ProductService {
+
+    private final ProductDAO dao;
+
+    public ProductService(ProductDAO dao) {
+        this.dao = dao;
+    }
+
+    public int getMaxID() {
+        try {
+            int maxID = dao.getMaxID();
+
+            return maxID;
+        } catch (Exception e) {
+            System.out.println("Erro ao obter o máximo ID: " + e.getMessage());
+            return -1;
+        }
+
+    }
+
+    public boolean create(String name, String description, int quantity, double price) {
+        try {
+            if (name == null || name.isEmpty() || name.length() < 2) {
+                throw new IllegalArgumentException("Nome do produto não pode ser nulo, vazio ou menor que dois caracteres");
+            }
+
+            if (description == null || description.isEmpty() || description.length() < 10) {
+                throw new IllegalArgumentException("Descrição do produto não pode ser nula, vazia ou menor que dez caracteres");
+            }
+
+            if (quantity <= 0) {
+                throw new IllegalArgumentException("Quantidade tem que ser maior que zero");
+            }
+            if (price <= 0) {
+                throw new IllegalArgumentException("Preço tem que ser maior que zero");
+            }
+
+            int id = this.getMaxID() + 1;
+
+            Date date = new Date();
+
+            Product product = new Product(id, name, description, quantity, price, date);
+
+            boolean isCreated = dao.create(product);
+            if (isCreated) {
+                System.out.println("Produto cadastrado com sucesso!");
+                return true;
+            } else {
+                System.out.println("Falha ao cadastrar o produto.");
+                return false;
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro na validação dos dados informados: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Erro ao criar o produto: " + e.getMessage());
+            return false;
+        }
+    }
+}
