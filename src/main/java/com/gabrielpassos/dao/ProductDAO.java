f4f12e6ed8111ca1262342b647b87468400ab805
Rodrigo Luigi <digaor@gmail.com>
2024-05-20 21:25:27 -0300

    add: methods from ProductDAO

--- src/main/java/com/gabrielpassos/dao/ProductDAO.java
+++ src/main/java/com/gabrielpassos/dao/ProductDAO.java
@@ -7,13 +7,39 @@ import com.gabrielpassos.repositories.ProductRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductDAO implements ProductRepository{
-    
+public class ProductDAO implements ProductRepository {
+
     private final MySqlDriver db;
     private final List<Product> productList;
 
     public ProductDAO() {
         this.db = new MySqlDriver();
         this.productList = new ArrayList<>();
+    }
+
+    @Override
+    public Product findById(String name) {
+        return null;
+
+    }
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public boolean create(Product product) {
+        return true;
+    }
+
+    @Override
+    public boolean update(Product product) {
+        return true;
+    }
+
+    @Override
+    public boolean delete(Product product) {
+        return true;
     }
 }
